0b0fcf5684ac3a01befc76ec37296230bbdae0ba
Cristhian <cristhianmartin12@gmail.com>
2018-11-05 14:33:02 -0500

    REST Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,19 @@
 //  Puerto
 // ==============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==============================
+//  Entorno
+// ==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============================
+//  Entorno
+// ==============================
+let urlDB = '';
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:abcDEF123@ds121282.mlab.com:21282/cafe';
+}
+process.env.URL_DB = urlDB;
